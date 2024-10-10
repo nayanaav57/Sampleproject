c7f0576c3e60841f20aa09e7152b667296986cb4
nayanaav57 <nayanaav57@gmail.com>
2024-10-10 20:56:01 +0530

    my initial commit

---
+++ Area.java
@@ -0,0 +1,12 @@
+class Area
+{
+public static void main(String args[])
+{
+int b=25;
+int h=15;
+System.out.println("base = " +(b));
+System.out.println("height = " +(h));
+float area = 0.5f*b*h;
+System.out.println("Area = " +(area));
+}
+}
