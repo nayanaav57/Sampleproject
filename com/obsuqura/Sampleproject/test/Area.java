c296394d3da66fb9b2a210fe667eaeb7f5817dc6
nayanav57 <nayanaav57@gmail.com>
2024-10-11 06:48:39 +0530

    initial commit using eclipse

--- com/obsuqura/Sampleproject/test/Area.java
+++ com/obsuqura/Sampleproject/test/Area.java
@@ -1,9 +1,11 @@
+package com.obsuqura.Sampleproject.test;
+
 class Area
 {
 public static void main(String args[])
 {
-int b=25;
-int h=15;
+float b=25;
+float h=15;
 System.out.println("base = " +(b));
 System.out.println("height = " +(h));
 float area = 0.5f*b*h;
---
+++ com/obsuqura/Sampleproject/test/Average.java
@@ -0,0 +1,22 @@
+package com.obsuqura.Sampleproject.test;
+
+class Average
+{
+
+public void avg(int a, int b,int c)
+{
+
+float d=(a+b+c)/2;
+System.out.println(d);
+}
+
+public static void main(String args[])
+{
+
+Average obj= new Average();
+obj.avg(10,20,30);
+
+}
+
+}
+
